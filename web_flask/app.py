7a45ca43f0c2f77a27ca38567c0337d9f9ac8d7a
Carlos Zuluaga <carlosez9@hotmail.com>
2020-02-01 09:22:57 -0500

    Add index static file and templates. Start route on Flask implemented

--- web_flask/app.py
+++ web_flask/app.py
@@ -3,13 +3,13 @@
 starts a Flask web application
 """
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return "Hello World!"
+def fetch_index():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
