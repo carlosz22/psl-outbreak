3d2f3309ec7cf642568af9fe1d6e5c0ac756fb68
Carlos Zuluaga <carlosez9@hotmail.com>
2020-02-01 19:02:37 -0500

    Fix __session on dbengine

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -56,7 +56,7 @@ class DBStorage:
     def reload(self):
         """reloads data from the database"""
         Base.metadata.create_all(self.__engine)
-        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        sess_factory = sessionmaker(bind=self.engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
         self.session = Session
 
