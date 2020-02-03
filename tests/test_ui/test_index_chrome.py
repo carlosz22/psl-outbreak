5c48572685e314289b62756b7136225992c5a0ce
Carlos Zuluaga <carlosez9@hotmail.com>
2020-02-03 01:01:11 -0500

    Feature: Add tests for report page

--- tests/test_ui/test_index_chrome.py
+++ tests/test_ui/test_index_chrome.py
@@ -16,12 +16,12 @@ class TestIndexChrome(unittest.TestCase):
 
     def test_summary_data(self):
         """Go to find out more on summary section"""
-        self.driver.get('http://psl-outbreak.herokuapp.com')
+        self.driver.get('http://psl-outbreak.herokuapp.com/report')
         self.driver.find_element_by_id('summary_find_out').click()
 
     def test_summary_report(self):
         """Go to report cases on summary section"""
-        self.driver.get('http://psl-outbreak.herokuapp.com')
+        self.driver.get('http://psl-outbreak.herokuapp.com/report')
         self.driver.find_element_by_id('summary_report_cases').click()
 
     @classmethod
